aaad7721bc6c985059ef787114e2872082d96e3c
L7Y379 <1102907282@qq.com>
2021-10-13 10:03:05 +0800

    update

--- boot-05-web-admin/src/main/java/com/atguigu/admin/Boot05WebAdminApplication.java
+++ boot-05-web-admin/src/main/java/com/atguigu/admin/Boot05WebAdminApplication.java
@@ -2,10 +2,11 @@ package com.atguigu.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan(basePackages = "com.atguigu.admin")
 @SpringBootApplication
 public class Boot05WebAdminApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Boot05WebAdminApplication.class, args);
     }
