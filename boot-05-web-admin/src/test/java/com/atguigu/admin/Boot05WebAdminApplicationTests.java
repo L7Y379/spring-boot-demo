35df947efb22d421e0bb94ebd609b16f8c1178aa
L7Y379 <1102907282@qq.com>
2021-10-18 22:06:39 +0800

    update

--- boot-05-web-admin/src/test/java/com/atguigu/admin/Boot05WebAdminApplicationTests.java
+++ boot-05-web-admin/src/test/java/com/atguigu/admin/Boot05WebAdminApplicationTests.java
@@ -1,13 +1,24 @@
 package com.atguigu.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.jdbc.core.JdbcTemplate;
+@Slf4j
 @SpringBootTest
 class Boot05WebAdminApplicationTests {
 
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+        jdbcTemplate.queryForList("select * from myguests");
+        //Long aLong =jdbcTemplate.queryForObject("select count (*) from myguests",long.class);
+        //Long aa=jdbcTemplate.queryForObject("select count * from myguests",long.class);
+        //log.info("记录总数：{}",aa);
     }
 
 }
